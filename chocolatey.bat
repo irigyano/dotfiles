fcde2d8909e364e030d9e9986391530f7e3457d3
irigyano <110440243+irigyano@users.noreply.github.com>
2023-11-08 17:55:09 +0800

    KEKWait

--- chocolatey.bat
+++ chocolatey.bat
@@ -1,17 +1,14 @@
 @echo off
 
-echo make sure run this script as "Administrator" with "Powershell" on "Windows".
+echo make sure run this script as ADMINISTRATOR!!
 @pause
 
 echo Installing Chocolatey
 powershell Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 echo Installing packages
-choco install git -y
-choco install nodejs-lts -y
 choco install 7zip -y
 choco install googlechrome -y
-choco install microsoft-windows-terminal -y
-choco install neovim -y
+choco install vscode -y
 
 @pause
